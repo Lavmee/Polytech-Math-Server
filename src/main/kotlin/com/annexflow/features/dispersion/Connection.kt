1b8536ed0411814da5728091f10a818913ff97d1
Lavmee <66745577+Lavmee@users.noreply.github.com>
2022-11-09 16:50:52 +0300

    feature: Add Websocket dispersion feature.

--- src/main/kotlin/com/annexflow/features/dispersion/Connection.kt
+++ src/main/kotlin/com/annexflow/features/dispersion/Connection.kt
@@ -2,4 +2,4 @@ package com.annexflow.features.dispersion
 
 import io.ktor.server.websocket.*
 
-class Connection(val session: WebSocketServerSession) 
+class Connection(val session: WebSocketServerSession)
